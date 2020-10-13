e8e59a2030dbcde8224976bed3718f16bf27acf2
Daz DeBoer <daz@gradle.com>
2020-10-13 16:46:01 -0600

    Demonstrate issue without `Enclosed` test

--- src/test/java/com/test/RunAllSuiteTest.java
+++ src/test/java/com/test/RunAllSuiteTest.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ EnclosedTest.class })
+@SuiteClasses({ SimpleTest.class })
 public class RunAllSuiteTest
 {
 }
---
+++ src/test/java/com/test/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTest {
+    @Test
+    public void theTest() throws InterruptedException {
+        Thread.sleep(100);
+    }
+}
